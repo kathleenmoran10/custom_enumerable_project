af21811fd8382e21ad5519e4bf1f3ebd549e398b
Kathleen Moran <kathleenmoran10@gmail.com>
2022-05-17 18:35:30 -0400

    Add my_each, each_with_index, and my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,22 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |element|
+      yield element, index
+      index += 1
+    end
+    self
+  end
+
+  def my_select
+    selected_elements = []
+    my_each do |element|
+      if yield element
+        selected_elements << element
+      end
+    end
+    selected_elements
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +24,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 end
