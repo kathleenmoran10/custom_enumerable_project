f061d7b35bbe014f7ef08a7f13326688dee0b969
Kathleen Moran <kathleenmoran10@gmail.com>
2022-05-18 10:25:18 -0400

    Add my_all?, my_any?, my_none?, my_count, my_map, and my_inject

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -17,6 +17,66 @@ module Enumerable
     end
     selected_elements
   end
+
+  def my_all?
+    all = true
+    my_each do |element|
+      result = yield element
+      all &&= result
+    end
+    all
+  end
+
+  def my_any?
+    any = false
+    my_each do |element|
+      result = yield element
+      any ||= result
+    end
+    any
+  end
+
+  def my_none?
+    none = true
+    my_each do |element|
+      result = yield element
+      none &&= !result
+    end
+    none
+  end
+
+  def my_count
+    count = 0
+    my_each do |element|
+      count += if block_given?
+                 (yield element) ? 1 : 0
+               else
+                 1
+               end
+    end
+    count
+  end
+
+  def my_map
+    mapped_elements = []
+    my_each do |element|
+      mapped_elements << (yield element)
+    end
+    mapped_elements
+  end
+
+  def my_inject(base = nil)
+    reduction = nil
+    first_element = true
+    my_each do |element|
+      if first_element
+        reduction = base.nil? ? element : base
+        first_element = false
+      end
+      reduction = yield reduction, element
+    end
+    reduction
+  end
 end
 
 # You will first have to define my_each
